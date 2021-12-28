435df7d8e66dc5410da9e6e843db0383a21d45eb
BrenoRev <silvabreno462@gmail.com>
2021-12-28 04:53:18 -0300

    Starting form authenticate

--- src/main/java/com/dev/rev/security/ApplicationSecurityConfig.java
+++ src/main/java/com/dev/rev/security/ApplicationSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -62,7 +63,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic();
+			// Form auth
+			.formLogin();
+
 	}
 
 	@Override
